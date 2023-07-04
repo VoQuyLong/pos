1ef7ddfca6105b03615dea7d6b9ea8e15b18bb2e
Võ Qúy Long <long@idctechno.com>
2023-07-04 15:52:57 +0700

    seeding user data

---
+++ src/database/seeds/user/user-generator.service.ts
@@ -0,0 +1,69 @@
+import { User } from 'src/users/entities/user.entity';
+import { faker } from '@faker-js/faker';
+import { genders } from '../gender/gender.data';
+import { telecomProviders } from '../telecom/telecom.data';
+import { jobs } from '../job/job.data';
+import { districts } from '../district/district.data';
+import { Gender } from 'src/genders/entities/gender.entity';
+import { Job } from 'src/jobs/entities/job.entity';
+import { TelecomProvider } from 'src/telecoms/entities/telecom.entity';
+import { District } from 'src/districts/entities/district.entity';
+
+export class UserGenerator {
+  private static randomIndex(
+    source: Array<{ id: number; name: string }>,
+  ): number {
+    const index: number = faker.number.int({ min: 1, max: source.length - 1 });
+    return index;
+  }
+
+  public static createRandomUser(): User {
+    // Generate a random index
+    const randomGenderIndex = UserGenerator.randomIndex(genders);
+    const randomProviderIndex = UserGenerator.randomIndex(telecomProviders);
+    const randomJobIndex = UserGenerator.randomIndex(jobs);
+    const randomDistrictIndex = UserGenerator.randomIndex(districts);
+
+    const user: User = new User();
+    user.email = faker.internet.email();
+    user.password = faker.internet.password();
+    user.firstName = faker.person.firstName();
+    user.lastName = faker.person.lastName();
+    user.password = 'secret';
+    user.role = 'client';
+    user.status = 'active';
+    user.createdAt = faker.date.past();
+    user.updatedAt = faker.date.past();
+
+    const gender: Gender = new Gender();
+    gender.id = randomGenderIndex;
+    gender.name = genders[randomGenderIndex].name;
+    user.gender = gender;
+
+    const job: Job = new Job();
+    job.id = randomJobIndex;
+    job.name = jobs[randomJobIndex].name;
+    user.job = job;
+
+    const provider: TelecomProvider = new TelecomProvider();
+    provider.id = randomProviderIndex;
+    provider.name = telecomProviders[randomProviderIndex].name;
+    user.provider = provider;
+
+    const address: District = new District();
+    address.id = randomDistrictIndex;
+    address.name = districts[randomDistrictIndex].name;
+    user.address = address;
+
+    return user;
+  }
+
+  public static generateUsers(count: number): User[] {
+    const users: User[] = [];
+    for (let i = 0; i < count; i++) {
+      const user = UserGenerator.createRandomUser();
+      users.push(user);
+    }
+    return users;
+  }
+}
---
+++ src/database/seeds/user/user-seed.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { UserGenerator } from './user-generator.service';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserSeedService {
+  constructor(
+    @InjectRepository(User)
+    private repository: Repository<User>,
+  ) {}
+
+  async run() {
+    const countAdmin = await this.repository.count({
+      where: {
+        role: 'admin',
+      },
+    });
+
+    if (!countAdmin) {
+      const adminUser = this.repository.create({
+        firstName: 'Long',
+        lastName: 'VQ',
+        email: 'long@example.com',
+        password: 'secret',
+        role: 'admin',
+        status: 'active',
+      });
+      await this.repository.save(adminUser);
+    }
+
+    const countUser = await this.repository.count({
+      where: {
+        role: 'client',
+      },
+    });
+
+    if (!countUser) {
+      const users: User[] = UserGenerator.generateUsers(500);
+      const length: number = users.length;
+      for (let i = 0; i < length; i++) {
+        await this.repository.save(users[i]);
+      }
+    }
+  }
+}
