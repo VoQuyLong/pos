577083b7f827dddd1e4041ab05e66d2834bedb8d
Võ Qúy Long <long@idctechno.com>
2023-07-05 15:39:50 +0700

    add graphql and swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { SessionModule } from './sessions/session.module';
+import { SessionModule } from './session/session.module';
+import { TelecomModule } from './telecoms/telecoms.module';
 import databaseConfig from './config/database.config';
 import authConfig from './config/auth.config';
 import appConfig from './config/app.config';
@@ -9,6 +10,11 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './database/typeorm-config.service';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GenderModule } from './genders/genders.module';
+import { JobModule } from './jobs/jobs.module';
+import { DistrictModule } from './districts/districts.module';
 
 @Module({
   imports: [
@@ -23,6 +29,15 @@ import { DataSource, DataSourceOptions } from 'typeorm';
         return new DataSource(options).initialize();
       },
     }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      installSubscriptionHandlers: true,
+    }),
+    DistrictModule,
+    JobModule,
+    GenderModule,
+    TelecomModule,
     UsersModule,
     AuthModule,
     SessionModule,
