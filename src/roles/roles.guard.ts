292c71d3f89efae76ec2e621d289e17ca70c0e73
Võ Qúy Long <long@idctechno.com>
2023-07-05 15:40:48 +0700

    update roles guard

---
+++ src/roles/roles.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.getAllAndOverride<string[]>('roles', [
+      context.getClass(),
+      context.getHandler(),
+    ]);
+    if (!roles.length) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+
+    console.log(request.user?.role);
+    return roles.includes(request.user?.role);
+  }
+}
